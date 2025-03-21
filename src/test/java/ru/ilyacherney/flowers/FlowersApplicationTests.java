de3cc27bd74411fae042eb57f5bad9d8216f9b02
Ilya Cherney <ilyacherney@yandex.ru>
2025-03-21 21:37:15 +0300

    Init

---
+++ src/test/java/ru/ilyacherney/flowers/FlowersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilyacherney.flowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
