4dfff4e2e3fb27f930e5071690baf528fc680c67
Ilya Cherney <ilyacherney@yandex.ru>
2025-03-25 21:36:14 +0300

    FLWR-13: Add basic flowers menu UI

--- src/main/java/ru/ilyacherney/flowers/FlowersApplication.java
+++ src/main/java/ru/ilyacherney/flowers/FlowersApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import ru.ilyacherney.flowers.cultivar.Cultivar;
-import ru.ilyacherney.flowers.cultivar.CultivarService;
-import ru.ilyacherney.flowers.flower.Flower;
-import ru.ilyacherney.flowers.flower.FlowerService;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class FlowersApplication {
@@ -19,12 +13,8 @@ public class FlowersApplication {
     }
 
     @Bean
-    CommandLineRunner runner(CultivarService cultivarService, FlowerService flowerService) {
+    CommandLineRunner runner() {
         return args -> {
-            Cultivar cultivar = cultivarService.createCultivar("Одуванчик", new BigDecimal(15.00));
-            Flower flower = flowerService.createFlower(cultivar);
-            System.out.println(flower);
-            System.out.println(cultivar);
         };
     }
 }
--- src/main/java/ru/ilyacherney/flowers/bot/Bot.java
+++ src/main/java/ru/ilyacherney/flowers/bot/Bot.java
@@ -2,25 +2,67 @@ package ru.ilyacherney.flowers.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.request.*;
+import com.pengrad.telegrambot.request.AnswerCallbackQuery;
+import com.pengrad.telegrambot.request.EditMessageReplyMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.ilyacherney.flowers.cultivar.Cultivar;
+import ru.ilyacherney.flowers.cultivar.CultivarService;
+
+import java.util.List;
 
 @Component
 public class Bot {
 
+    private final CultivarService cultivarService;
+
     @Value("${token}")
     String token;
+
+    public Bot(CultivarService cultivarService) {
+        this.cultivarService = cultivarService;
+    }
 
     @PostConstruct
     public void start() {
         TelegramBot bot = new TelegramBot(token);
         bot.setUpdatesListener(updates -> {
                 updates.forEach(update -> {
-                    long chatId = update.message().chat().id();
-                    SendResponse response = bot.execute(new SendMessage(chatId,  update.message().text()));
+                    if (update.message() != null) {
+                        long chatId = update.message().chat().id();
+                        SendMessage message = new SendMessage(chatId, "Меню");
+                        message.parseMode(ParseMode.HTML);
+                        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(
+                                new InlineKeyboardButton[]{
+                                        new InlineKeyboardButton("Цветы").callbackData("flowers"),
+                                });
+                        message.replyMarkup(keyboard);
+                        SendResponse response = bot.execute(message);
+                        System.out.println(response);
+                    }
+                    // Обработка callback-запросов
+                    if (update.callbackQuery() != null) {
+                        long chatId = update.callbackQuery().message().chat().id();
+                        String callbackData = update.callbackQuery().data();
+                        if ("flowers".equals(callbackData)) {
+                            InlineKeyboardMarkup flowersKeyboard = new InlineKeyboardMarkup();
+                            List<Cultivar> cultivars = cultivarService.getAllCultivars();
+                            for (Cultivar cultivar : cultivars) {
+                                InlineKeyboardButton[] button = {new InlineKeyboardButton(cultivar.getName()).callbackData(cultivar.getName())};
+                                flowersKeyboard.addRow(button);
+                            }
+                            // Редактирование сообщения (только кнопки)
+                            EditMessageReplyMarkup editMessage = new EditMessageReplyMarkup(chatId, update.callbackQuery().message().messageId())
+                                    .replyMarkup(flowersKeyboard);
+
+                            bot.execute(editMessage);
+                            bot.execute(new AnswerCallbackQuery(update.callbackQuery().id())); // Подтверждение callback'а
+                        }
+                    }
                 });
                 return UpdatesListener.CONFIRMED_UPDATES_ALL;
         }, e -> {
